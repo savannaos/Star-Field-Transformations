be60bdee307f7056b509695fea832b1e9eda17ef
savannaos <savanna.smith@uconn.edu>
2017-05-25 14:46:46 -0600

    frame generating scripts

--- data/frame_gen.cpp
+++ data/frame_gen.cpp
@@ -5,9 +5,9 @@
 #include <iomanip>
 #include "star.hpp"
 /*  Savanna Smith, 5/25/17
-    Generates first frame */
+    Generates frames for star data */
 
-/* read each line of input file and generate star object*/
+/* read each line of input file and generate star object to save in dictionary*/
 void initStars(std::map<double, Star>& starDict){
   std::ifstream input("star_data.txt");
   double theta,phi,mag;
@@ -23,16 +23,18 @@ void initStars(std::map<double, Star>& starDict){
 
 /* generate frame for stars with:
   - theta between (pi/2)-(dtheta/2) and (pi/2)+(dtheta/2)
-  - phi between zero and dphi */
-void genFrame(double dphi, const std::map<double, Star>& starDict){
-  double dTheta = M_PI/10;
+  - phi first between minPhi and minPhi + dphi*/
+void genFrame(double minPhi, const std::map<double, Star>& starDict){
+  double dTheta = M_PI/10, dPhi = M_PI/10;
   double minTheta = M_PI/2 - (dTheta/2);
   double maxTheta = M_PI/2 + (dTheta/2);
+  double maxPhi   = minPhi + dPhi;
   std::cout << "Showing stars with theta between "<< minTheta << " and "
-            << maxTheta << " for phi between 0 and " << dphi << std::endl;
+            << maxTheta << " for phi between " << minPhi << " and " << maxPhi << std::endl;
   for(auto p: starDict){
+    if(p.first > maxPhi) return; //since sorted by phi don't look at anything above max
     if(p.second.theta >= minTheta && p.second.theta <= maxTheta &&
-       p.second.phi >= 0 && p.second.phi <= dphi){
+       p.second.phi >= minPhi && p.second.phi <= maxPhi){
          std::cout << p.second.theta << " " << p.second.phi
                    << " " << p.second.mag << std::endl;
        }
@@ -42,7 +44,12 @@ void genFrame(double dphi, const std::map<double, Star>& starDict){
 int main(){
  std::map<double, Star> starDict;
  initStars(starDict);
- double dphi = M_PI/10;
- genFrame(dphi, starDict);
+ double minPhi = 0;
+ for(int i = 0; i < 20; i++){
+   std::cout << "Frame " << i << ": ";
+    genFrame(minPhi, starDict);
+    std::cout << std::endl;
+    minPhi += M_PI/100; //increment phi window
+ }
  return 0;
 }
