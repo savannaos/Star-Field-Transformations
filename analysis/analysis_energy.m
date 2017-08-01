03c65a27069028b5c6f868c6e8c508d241e838bc
Savanna Smith <savsmit4@gmail.com>
2017-08-01 09:33:43 -0600

    energy zoomed in before spike

--- analysis/analysis_energy.m
+++ analysis/analysis_energy.m
@@ -15,12 +15,12 @@ mkdir(out_folder);
 parent_output_dir = '/nh/compneuro/scratch/starOut/starField_SPM_9X9X16_5_500/train17-29-17';
 output_dir = parent_output_dir;
 
-title_str = ['energy'];
+title_str = ['energy_zoomed'];
 fh_energy = figure("name", title_str);
 cmap = colormap("prism");
 for i_batch = 1:nbatch
   energy = dlmread([parent_output_dir, filesep, './EnergyProbe_batchElement_', num2str(i_batch-1), '.txt'])(2:end,3);
-  plot(energy(steps:steps:end), "color", cmap(i_batch,:));
+  plot(energy(steps:steps:25000), "color", cmap(i_batch,:));
   hold on;
   %saveas(fh_energy, [out_folder, filesep, title_str, '_', num2str(i_batch), '.png'], 'png');
 endfor
