56a6afe087d1591fb8fe11f942692a91bf96371e
Savanna <savsmit4@gmail.com>
2017-08-01 13:32:29 -0600

    analysis run

--- analysis/analysis_singleLayer.m
+++ analysis/analysis_singleLayer.m
@@ -9,9 +9,9 @@ setenv("GNUTERM","X11")
 nbatch = 8;
 batchwidth = 1;
 
-parent_output_dir = '/nh/compneuro/scratch/starOut/starField_SPM_9X9X16_5_500/train17-29-17';
+parent_output_dir = '/nh/compneuro/scratch/starOut/starField_SPM_9X9X16_5_500/train18-01-17';
 output_dir = parent_output_dir;
-write_to = '~/Star-Field-Transformations/starOut/7-29-17/';
+write_to = '~/Star-Field-Transformations/starOut/8-01-17/';
 mkdir(write_to);
 i_batchsweep = 1;
 batchsweep_format_str = "%02d";
