e4135cd75615e662d08fc5b80c0f8ee70d588584
Savanna Smith <savsmit4@gmail.com>
2017-08-01 09:30:11 -0600

    energy

--- analysis/analysis_energy.m
+++ analysis/analysis_energy.m
@@ -10,9 +10,9 @@ nbatch = 8;
 batchwidth = 1;
 steps = 100; 
 
-out_folder = '~/Star-Field-Transformations/starOut/7-28-17';
+out_folder = '~/Star-Field-Transformations/starOut/7-29-17';
 mkdir(out_folder);
-parent_output_dir = '/nh/compneuro/scratch/starOut/starField_SPM_9X9X16_5_500/train17-28-17';
+parent_output_dir = '/nh/compneuro/scratch/starOut/starField_SPM_9X9X16_5_500/train17-29-17';
 output_dir = parent_output_dir;
 
 title_str = ['energy'];
@@ -20,7 +20,7 @@ fh_energy = figure("name", title_str);
 cmap = colormap("prism");
 for i_batch = 1:nbatch
   energy = dlmread([parent_output_dir, filesep, './EnergyProbe_batchElement_', num2str(i_batch-1), '.txt'])(2:end,3);
-  plot(energy(1:steps:end), "color", cmap(i_batch,:));
+  plot(energy(steps:steps:end), "color", cmap(i_batch,:));
   hold on;
   %saveas(fh_energy, [out_folder, filesep, title_str, '_', num2str(i_batch), '.png'], 'png');
 endfor
